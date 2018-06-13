16d3083a206ed470f203bbe3205bccd6f533b3e5
YifengGuo <gyf940517@gmail.com>
2018-06-13 16:28:51 -0400

    helper exception

---
+++ src/main/java/NoSuchRouteException.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by guoyifeng on 6/12/18.
+ */
+public class NoSuchRouteException extends Exception {
+    public NoSuchRouteException(String msg) {
+        super(msg);
+    }
+}
